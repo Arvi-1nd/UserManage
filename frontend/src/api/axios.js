2e5b2ab1505e0a598713538a1cb2d2c61e4c0e09
Arvi-1nd <davidarvind44@gmail.com>
2025-12-31 21:03:46 +0530

    axios added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://usermanage-production.up.railway.app/api/",
 });
 
-
+// Attach token automatically
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
+  const token = localStorage.getItem("access");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
